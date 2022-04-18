8940e7939f50ebd54f8c9dd6224793dc62b19f49
yaguruma <yaguruma_y@yahoo.co.jp>
2022-04-19 01:15:51 +0900

    [add]tutorial:テーブルの作成およびコマンドの定義

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,9 @@
+from pydoc import cli
 import sqlite3
 
+import click
 from flask import current_app, g
+from flask.cli import with_appcontext
 
 
 def get_db():
@@ -17,3 +20,17 @@ def close_db():
     db = g.pop('db', None)
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
