873358a617a7aacf800cbcc23878226bed3482e6
yaguruma <yaguruma_y@yahoo.co.jp>
2022-04-23 22:31:34 +0900

    [add]#12 get_db()のテストを作成

---
+++ tests/test_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+import pytest
+
+from flaskr.db import get_db
+
+
+def test_get_close_db(app):
+    # 同じコンテキスト内ではget_db()は常に同じ接続を返す
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+    
+    # pytest.raises(exception)  異常系のテスト
+    # sqlite3にエラーが生じた時、出力されるエラーの文言に'closed'が含まれている
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+    assert 'closed' in str(e.value)
