6014abdfbb36535dbe2fee2672e0ad3615142d82
yaguruma <yaguruma_y@yahoo.co.jp>
2022-04-30 16:41:48 +0900

    [update]#19 ログイン時のテストを追加

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,5 @@
 import pytest
-from flask import testing, Flask, session
+from flask import testing, Flask, session, g
 
 from flaskr.db import get_db
 
@@ -31,6 +31,14 @@ def test_register_validate(client: testing.FlaskClient, username, password, mess
 
 def test_login(client: testing.FlaskClient):
     assert client.get('/auth/login').status_code == 200
+    res = client.post(
+        '/auth/login', data={'username': 'test', 'password': 'test'})
+    assert res.headers['Location'] == '/'
+
+    with client:
+        client.get('/')
+        assert session['user-id'] == 1
+        assert g.user['username'] == 'test'
 
 
 def test_logout(client: testing.FlaskClient):
