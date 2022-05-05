685dea35a53c85970f2821a068ecf9b2a787256e
yaguruma <yaguruma_y@yahoo.co.jp>
2022-05-05 20:41:15 +0900

    [add]#29 投稿一覧ページにコメント数の取得及び表示を追加

--- flaskr/routes/blog.py
+++ flaskr/routes/blog.py
@@ -12,9 +12,13 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        'SELECT p.id, p.title, p.body, p.created, p.author_id, u.username, COALESCE(c.cnt, 0) AS comment_count '
+        ' FROM post AS p '
+        ' INNER JOIN user AS u '
+        ' ON p.author_id = u.id '
+        ' LEFT OUTER JOIN (SELECT post_id, count(*) AS cnt FROM comment GROUP BY post_id) AS c '
+        ' ON p.id = c.post_id '
+        ' ORDER BY p.created DESC; '
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
